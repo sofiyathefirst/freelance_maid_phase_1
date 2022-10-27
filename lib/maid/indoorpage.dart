551c5518b736fba77dbe53cee6e55fd917d5a387
sofiyathefirst <69139655+sofiyathefirst@users.noreply.github.com>
2022-10-28 00:31:48 +0800

    Changes to be committed:     new file:   lib/maid/indoorpage.dart  Changes not staged for commit: 	modified:   lib/customer/cust_homepage.dart 	modified:   lib/customer/cust_profilepage.dart 	modified:   lib/customer/cust_register.dart 	deleted:    lib/splash_screen.dart  Untracked files: 	lib/common method/deleteuser.dart 	lib/customer/cust_editprofile.dart 	lib/maid/outdoorpage.dart

---
+++ lib/maid/indoorpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IndooPage extends StatefulWidget {
+  IndooPage({Key? key}) : super(key: key);
+
+  @override
+  State<IndooPage> createState() => _IndooPageState();
+}
+
+class _IndooPageState extends State<IndooPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
